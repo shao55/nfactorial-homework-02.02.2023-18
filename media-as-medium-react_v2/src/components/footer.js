6275bd158fe8503c7624a63dde580ec9592b282f
Nurgissa Itkarinov <nurgissa.22@gmail.com>
2023-02-03 11:40:50 +0600

    day 2

--- media-as-medium-react_v2/src/components/footer.js
+++ media-as-medium-react_v2/src/components/footer.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Footer = () => {
     return <footer>
-    <div class="container">
-        <div class="footer_main">
-            <div class="footer_description">
-              <p class="footer_description_text">Credits: photos from <a href="https://unsplash.com/@surface" class="footer_description_text_links">Unsplash.com</a>, icons from <a href="https://icons8.com/" class="footer_description_text_links">Icons8</a>, graphics from <a href="https://craftwork.design/checkout/purchase-confirmation/?payment_key=a3ba0d28aa30dfd691cd535acb1f4228" class="footer_description_text_links">craftwork.design</a>.</p>	
+    <div className="container">
+        <div className="footer_main">
+            <div className="footer_description">
+              <p className="footer_description_text">Credits: photos from <a href="https://unsplash.com/@surface" className="footer_description_text_links">Unsplash.com</a>, icons from <a href="https://icons8.com/" className="footer_description_text_links">Icons8</a>, graphics from <a href="https://craftwork.design/checkout/purchase-confirmation/?payment_key=a3ba0d28aa30dfd691cd535acb1f4228" className="footer_description_text_links">craftwork.design</a>.</p>	
             </div>
-            <div class="footer_end">
-              <p class="footer_end_text">Made with ✨❤️ at nFactorial in 2022.</p>
+            <div className="footer_end">
+              <p className="footer_end_text">Made with ✨❤️ at nFactorial in 2022.</p>
           </div>
         </div>
     </div>
--- media-as-medium-react_v2/src/media_as_medium.js
+++ media-as-medium-react_v2/src/media_as_medium.js
@@ -1,14 +1,37 @@
-
 import './media_as_medium.css';
 import Header from './components/header';
 import Main from './components/main';
 import Footer from './components/footer';
+import index_1 from './components/img/index_1.png';
+import index_2 from './components/img/index_2.png';
+import index_3 from './components/img/index_3.png';
 
 const media_as_medium = () => {
+
+  const articles = [
+    {
+      head: "Article 1",
+      content: "This is the content of article 1",
+      img: index_1
+    },
+    {
+      head: "Article 2",
+      content: "This is the content of article 2",
+      img: index_2
+    },
+    {
+      head: "Article 3",
+      content: "This is the content of article 3",
+      img: index_3
+    }
+  ];
+
   return (
     <body>
       <Header />
-      <Main />
+      {
+        articles.map((item, index) => <Main key={index} head={item.head} content={item.content} img={item.img}/>)
+      }
       <Footer />
     </body>
   );
